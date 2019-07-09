88fb6997598c490e6d631f7a1ee8ed7f17cd1813
Mykola Dvornik <mykola.dvornik@gmail.com>
2019-07-09 20:53:51 +0200

    process: increase sox buffer and enable sox mutlithreading

--- process.go
+++ process.go
@@ -43,7 +43,7 @@ const (
 )
 
 var (
-	soxArgs    = "%s -t wav -b %d %s gain %+.2g rate -a -R 198 -c 4096 -p 45 -t -b 95 %d dither"
+	soxArgs    = "--buffer 524288 --multi-threaded %s -t wav -b %d %s gain %+.2g rate -a -R 198 -c 4096 -p 45 -t -b 95 %d dither"
 	ffmpegArgs = "-y -hide_banner -i %s -af loudnorm=I=-24:LRA=14:TP=-4:print_format=json -f null /dev/null"
 	volArgs    = "%s -t wav -e signed-integer -b %d %s gain %+.2g dither"
 	homeDir, _ = os.UserHomeDir()
