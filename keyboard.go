0a97f05bb02e73908a962226cfe83700e6c40eb8
Mykola Dvornik <mykola.dvornik@gmail.com>
2019-07-11 22:36:50 +0200

    keyboard: more reliable processing

--- keyboard.go
+++ keyboard.go
@@ -2,11 +2,18 @@ package main
 
 import (
 	"os"
+	"time"
 
 	"github.com/eiannone/keyboard"
 )
 
 func processKeyboard() {
+	err := keyboard.Open()
+	if err != nil {
+		panic(err)
+	}
+	defer keyboard.Close()
+
 	for {
 		_, key, err := keyboard.GetSingleKey()
 		if err != nil {
@@ -18,5 +25,6 @@ func processKeyboard() {
 		if key == keyboard.KeyEsc {
 			os.Exit(0)
 		}
+		time.Sleep(500 * time.Millisecond)
 	}
 }
