d136e4400b7bc4be8001f381549acb3da0a421a4
Mykola Dvornik <mykola.dvornik@gmail.com>
2019-09-09 23:13:42 +0200

    `space` now toggles playback

--- keyboard.go
+++ keyboard.go
@@ -3,5 +3,16 @@ package main
 import "github.com/gdamore/tcell"
 
 func processKeyboard(event *tcell.EventKey) *tcell.EventKey {
+	switch {
+	case event.Rune() == ' ':
+		if device.IsStarted() {
+			device.Stop()
+		} else {
+			device.Start()
+		}
+		return nil
+	default:
+		return event
+	}
 	return event
 }
