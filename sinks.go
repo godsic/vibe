128125bd310adcbd996027789f080cd6e38bfe2a
Mykola Dvornik <mykola.dvornik@gmail.com>
2020-04-19 21:36:31 +0200

    sinks: add AKG K514 with equalization based on the data from github.com/jaakkopasanen/AutoEq

--- sinks.go
+++ sinks.go
@@ -9,6 +9,7 @@ import (
 var (
 	Sinks = []*Sink{
 		NewSink("AKG K514", 34.4, 116.9, ""),
+		NewSink("AKG K514 (equalized)", 34.4, 110.0, "gain -7 equalizer 39 0.34q 7.0 equalizer 330 0.25q -3.5 equalizer 895 0.67q -3.7 equalizer 4346 0.72q 6.8 equalizer 17370 0.5q -5.8 equalizer 1486 3.73q -2.6 equalizer 3475 0.57q 2.8 equalizer 3520 1.74q -3.7 equalizer 7986 3.55q -3.0 equalizer 11715 4.55q 1.1"),
 		NewSink("AKG K702", 67.0, 100.0, ""),
 		NewSink("Sennheiser HD4.30", 23.0, 116.0, ""),
 		NewSink("Sennheiser PX90", 35.3, 104.5, ""),
